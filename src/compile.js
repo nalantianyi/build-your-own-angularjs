dc529e0a8c183b529944e7bd5e52c0e8d0c5a2e4
Tero Parviainen <tero@teropa.info>
2014-12-27 13:35:26 +0100

    Attaching Directive Controllers on The Scope

--- src/compile.js
+++ src/compile.js
@@ -409,7 +409,7 @@ function $CompileProvider($provide) {
             if (controllerName === '@') {
               controllerName = attrs[directive.name];
             }
-            $controller(controllerName, locals);
+            $controller(controllerName, locals, directive.controllerAs);
           });
         }
 
