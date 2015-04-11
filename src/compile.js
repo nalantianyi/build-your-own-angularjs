54a20eb06c8810d74f672352705fd5c6c9be9612
Tero Parviainen <tero@teropa.info>
2015-04-11 12:59:12 +0300

    Linking Directives that Were Compiled Earlier

--- src/compile.js
+++ src/compile.js
@@ -428,7 +428,9 @@ function $CompileProvider($provide) {
       var $compileNode = $(compileNode);
       var terminalPriority = -Number.MAX_VALUE;
       var terminal = false;
-      var preLinkFns = [], postLinkFns = [], controllers = {};
+      var preLinkFns = previousCompileContext.preLinkFns || [];
+      var postLinkFns = previousCompileContext.postLinkFns || [];
+      var controllers = {};
       var newScopeDirective, newIsolateScopeDirective;
       var templateDirective = previousCompileContext.templateDirective;
       var controllerDirectives;
@@ -531,7 +533,11 @@ function $CompileProvider($provide) {
             _.drop(directives, i),
             $compileNode,
             attrs,
-            {templateDirective: templateDirective}
+            {
+              templateDirective: templateDirective,
+              preLinkFns: preLinkFns,
+              postLinkFns: postLinkFns
+            }
           );
           return false;
         } else if (directive.compile) {
