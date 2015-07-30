dc022151ea3951344dffdfc9b318e5f6336f04ca
Tero Parviainen <tero@teropa.info>
2015-07-30 11:09:14 +0300

    Filtering With Predicate Functions

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -1,6 +1,6 @@
 function filterFilter() {
-  return function() {
-
+  return function(array, filterExpr) {
+    return _.filter(array, filterExpr);
   };
 }
 
--- test/filter_filter_spec.js
+++ test/filter_filter_spec.js
@@ -8,4 +8,14 @@ describe("filter filter", function() {
     expect(filter('filter')).toBeDefined();
   });
 
+  it('can filter an array with a predicate function', function() {
+    var fn = parse('[1, 2, 3, 4] | filter:isOdd');
+    var scope = {
+      isOdd: function(n) {
+        return n % 2 !== 0;
+      }
+    };
+    expect(fn(scope)).toEqual([1, 3]);
+  });
+
 });
